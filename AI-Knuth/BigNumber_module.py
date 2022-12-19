1d8aeb0d6de66f52f9ddde061cb55816e497cbce
billtsol <billtsol@protonmail.com>
2022-12-19 19:34:17 +0200

    How to use Bignumber and mpMath

--- AI-Knuth/BigNumber_module.py
+++ AI-Knuth/BigNumber_module.py
@@ -1,32 +1,35 @@
-# How to use BigNimber Library in Python3.10
-
-import BigNumber 
+# How to use BigNimber Library in Python 3.10
+import mpmath as mp
+import BigNumber
 from BigNumber.BigNumber import factorial, sqrt
 
 
-number = 500000 # 500.000
+print("-----------------------BigNumber module-----------------------")
+number = 500000  # 500.000
 
-bigNumber = BigNumber.BigNumber.BigNumber(number) # Convert $number to BigNumber
+bigNumber = BigNumber.BigNumber.BigNumber(
+    number)  # Convert $number to BigNumber
 
-newBigNumber = BigNumber.BigNumber.BigNumber(factorial(bigNumber)) # $newBigNumber get the factorial of $bigNumber
+# $newBigNumber get the factorial of $bigNumber
+newBigNumber = BigNumber.BigNumber.BigNumber(factorial(bigNumber))
 
-print("Number: ", number, " ",number.__class__)
-print("BigNumber: ",bigNumber, " ", bigNumber.__class__)
+print("Number: ", number, " ", number.__class__)
+print("BigNumber: ", bigNumber, " ", bigNumber.__class__)
 print("Factorial of bigNumber: ", newBigNumber, " ", newBigNumber.__class__)
 
 
-root = sqrt(bigNumber) # Root of bigNumber
+root = sqrt(bigNumber)  # Root of bigNumber
 
-print("Root: ",root, " ",root.__class__)
+print("Root: ", root, " ", root.__class__)
 
-root = sqrt(newBigNumber) # Root of bigNumber factorial
+root = sqrt(newBigNumber)  # Root of bigNumber factorial
 
-print("Root of factorial: ",root, " ",root.__class__)
+print("Root of factorial: ", root, " ", root.__class__)
 
-for i in range(17): # 17 times root of newBigNumber
+for i in range(17):  # 17 times root of newBigNumber
     root = sqrt(root)
 
-print("Root of factorial: ",root, " ",root.__class__)
+print("Root of factorial: ", root, " ", root.__class__)
 
 # Floating point BigNumber
 floatBigNumber = BigNumber.BigNumber.BigNumber("0.34")
@@ -34,4 +37,38 @@ print("Float: ", floatBigNumber, " ", floatBigNumber.__class__)
 
 # Add in floating point bigNumber
 newFloatBigNumber = BigNumber.BigNumber.BigNumber("3") + floatBigNumber
-print("new Float: ", newFloatBigNumber, " ",newFloatBigNumber.__class__)
+print("new Float: ", newFloatBigNumber, " ", newFloatBigNumber.__class__)
+
+
+# How to use mpMath Library in Python 3.10
+print("-----------------------mpMath module-----------------------")
+
+bigNumber = 500000  # 500.000
+
+# $newBigNumber get the factorial of $bigNumber
+newBigNumber = mp.fac(bigNumber)
+
+print("Number: ", bigNumber, " ", bigNumber.__class__)
+print("Factorial of bigNumber: ", newBigNumber, " ", newBigNumber.__class__)
+
+
+root = mp.sqrt(bigNumber)  # Root of bigNumber
+
+print("Root: ", root, " ", root.__class__)
+
+root = mp.sqrt(newBigNumber)  # Root of bigNumber factorial
+
+print("Root of factorial: ", root, " ", root.__class__)
+
+for i in range(17):  # 17 times root of newBigNumber
+    root = mp.sqrt(root)
+
+print("Root of factorial: ", root, " ", root.__class__)
+
+# Floating point BigNumber
+floatBigNumber = 0.34
+print("Float: ", floatBigNumber, " ", floatBigNumber.__class__)
+
+# Add in floating point bigNumber
+newFloatBigNumber = mp.floor(floatBigNumber)
+print("mpMath floor: ", newFloatBigNumber, " ", newFloatBigNumber.__class__)
